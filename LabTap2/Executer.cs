4bb4aa056dd8aad3e20632001b9d58ae192bf1ad
Pietro Cattaneo <pieca314@gmail.com>
2018-10-25 12:44:50 +0200

    Primo commit

---
+++ LabTap2/Executer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MyAttribute;
+
+namespace LabTap2
+{
+    class Executer
+    {
+        static void Main(string[] args)
+        {
+         //Carico Dll
+            var a = Assembly.LoadFrom("MyLibrary.dll");
+            foreach (var type in a.GetTypes())
+            {
+            //    Console.WriteLine(type.FullName);
+                foreach (var m in type.GetMethods())
+                {
+                  //  Console.WriteLine(m.ToString());
+                }
+            }
+            //Invoco i metodi nella dll
+
+            var classi = a.GetTypes();
+
+            foreach (var classe in classi)
+            {
+                //Creo istanza del tipo
+                var _classInstance = Activator.CreateInstance(classe);
+                //Recupero metodi della classe
+                MethodInfo[] methods = classe.GetMethods();
+                //Per ogni metodo
+                foreach (var m in methods)
+                {
+                    //Recupero custom attributes
+                    var att = m.GetCustomAttributes<ExecuteMeAttribute>();
+                    //Per ogni custom attribute
+                    foreach (var cust in att)
+                    {
+                      //Recupero parametro attribute
+                      var par = cust.args;
+                      //Invoco il metodo con quel parametro
+                      m.Invoke(_classInstance, par);
+                        
+                    }
+                //    Console.WriteLine(m.ToString());
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyAttribute/ExecuteMeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAttribute
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class ExecuteMeAttribute : Attribute
+    {
+        public object[] args;
+
+        public ExecuteMeAttribute(params object[] args)
+        {
+            this.args = args;
+        }
+
+    }
+
+}
