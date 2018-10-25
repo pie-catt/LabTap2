207623d4e3bdcb6cef9aa78bbee9aac9f1e34722
Pietro Cattaneo <pieca314@gmail.com>
2018-10-25 13:16:25 +0200

    Commit seconda release

--- LabTap2/Executer.cs
+++ LabTap2/Executer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using MyAttribute;
@@ -10,16 +11,21 @@ namespace LabTap2
 {
     class Executer
     {
+        private static bool Notify(Exception e)
+        {
+            return false;
+        }
+
         static void Main(string[] args)
         {
-         //Carico Dll
-            var a = Assembly.LoadFrom("MyLibrary.dll");
+            //Carico Dll
+            var a = Assembly.LoadFrom("C:\\Users\\User\\source\\repos\\LabTap2\\MyLibrary\\bin\\Debug\\MyLibrary.dll");
             foreach (var type in a.GetTypes())
             {
-            //    Console.WriteLine(type.FullName);
+                //    Console.WriteLine(type.FullName);
                 foreach (var m in type.GetMethods())
                 {
-                  //  Console.WriteLine(m.ToString());
+                    //  Console.WriteLine(m.ToString());
                 }
             }
             //Invoco i metodi nella dll
@@ -29,29 +35,38 @@ namespace LabTap2
             foreach (var classe in classi)
             {
                 //Creo istanza del tipo
-                var _classInstance = Activator.CreateInstance(classe);
-                //Recupero metodi della classe
-                MethodInfo[] methods = classe.GetMethods();
-                //Per ogni metodo
-                foreach (var m in methods)
+                try
                 {
-                    //Recupero custom attributes
-                    var att = m.GetCustomAttributes<ExecuteMeAttribute>();
-                    //Per ogni custom attribute
-                    foreach (var cust in att)
+                    var _classInstance = Activator.CreateInstance(classe);
+
+
+                    //Recupero metodi della classe
+                    MethodInfo[] methods = classe.GetMethods();
+                    //Per ogni metodo
+                    foreach (var m in methods)
                     {
-                      //Recupero parametro attribute
-                      var par = cust.args;
-                      //Invoco il metodo con quel parametro
-                      m.Invoke(_classInstance, par);
-                        
+                        //Recupero custom attributes
+                        var att = m.GetCustomAttributes<ExecuteMeAttribute>();
+                        //Per ogni custom attribute
+                        foreach (var cust in att)
+                        {
+                            //Recupero parametro attribute
+                            var par = cust.args;
+                            //Invoco il metodo con quel parametro
+                            m.Invoke(_classInstance, par);
+
+                        }
+                        //    Console.WriteLine(m.ToString());
                     }
-                //    Console.WriteLine(m.ToString());
                 }
+
+                catch (System.MissingMethodException e) when (Notify(e))
+                {
+                }
+
+              // Console.ReadLine();
             }
 
-
-            Console.ReadLine();
         }
     }
 }
--- MyLibrary/Foo.cs
+++ MyLibrary/Foo.cs
@@ -9,23 +9,24 @@ namespace MyLibrary
 {
     public class Foo
     {
-    [ExecuteMe]
-    public void M1()
+        [ExecuteMe]
+        public void M1()
 
         {
             Console.WriteLine("M1");
         }
 
-    [ExecuteMe(45)]
-    [ExecuteMe(0)]
-    [ExecuteMe(3)]
-    public void M2(int a)
+        // [ExecuteMe(45)]
+        [ExecuteMe(0)]
+        [ExecuteMe(3)]
+        [ExecuteMe(88)]
+        public void M2(int a)
         {
             Console.WriteLine("M2 a={0}", a);
         }
 
-    [ExecuteMe("hello", "reflection")]
-    public void M3(string s1, string s2)
+        [ExecuteMe("hello", "reflection")]
+        public void M3(string s1, string s2)
         {
             Console.WriteLine("M3 s1={0} s2={1}", s1, s2);
         }
@@ -52,6 +53,22 @@ namespace MyLibrary
         public void M3(string s1, string s2)
         {
             Console.WriteLine("M3 s1={0} s2={1}", s1, s2);
+        }
+    }
+
+    public class Foo3
+    {
+
+        public Foo3(int a)
+        {
+            
+        }
+
+        [ExecuteMe]
+        public void M1()
+
+        {
+            Console.WriteLine("M1");
         }
     }
 }
